1b16b9ce597a6d52f60506512663db00c0f9dd98
harshiltomar <harshiltomar20@gmail.com>
2022-11-04 22:56:38 +0530

    velocity improvisation

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:codepur/core/store.dart';
 import 'package:codepur/pages/cart_page.dart';
 import 'package:codepur/pages/login_page.dart';
 import 'package:codepur/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'models/catalog.dart';
 import 'pages/home_page.dart';
 import 'package:codepur/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'core/store.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+      VxState(store: MyStore(MyStore.catalog, MyStore.cart), child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
